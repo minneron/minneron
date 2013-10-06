632de8282c941e96b77ea519b2f2dc23fe3a6740
tangentstorm <michal.wallace@gmail.com>
2013-10-06 11:43:27 -0500

    min: sql triggers to automatically maintain a closure table

--- minneron.sql
+++ minneron.sql
@@ -51,12 +51,123 @@ begin;
 commit;
 
 
-create table tree (
-  trid  integer references node,
-  above integer references node,
-  below integer references node,
-  depth integer
+
+-- tree_data contains the core data for ordered trees.
+create table tree_data (
+  tree   integer references node,  -- a node can appear in multiple trees
+  parent integer references node,
+  child  integer references node,
+  seq    real );
+
+-- tree_path contains automatically generated information
+-- about each node's full subtree.
+create table tree_path (
+  tree   integer references node,
+  above  integer references node,
+  below  integer references node,
+  steps  integer not null default 0
 );
+
+
+-- this is used by tree_del_node
+create table flags (
+  flag text primary key
+);
+
+create trigger tree_add_node after insert on tree_data
+  begin
+    insert into tree_path (tree, above, below, steps)
+        -- create a new path to itself:
+        select new.tree, new.child, new.child, 0
+      union all
+        -- copy the parent's path to the root:
+        select tree, above, new.child, steps + 1
+        from tree_path
+        where below = new.parent
+          and tree = new.tree;
+  end;
+
+-- i would have preferred to have this get created and
+-- destroyed inside the triggers, but sqlite doesn't
+-- allow create/drop statements inside triggers.
+create table subtree (nid integer);
+
+create trigger tree_del_node after delete on tree_data
+  when not exists (select * from flags where flag='recursive-tree-delete')
+  begin
+    -- this trigger deletes the whole subtree, which would cause
+    -- some harmless but slightly wasteful recursion.
+    insert into flags values ('recursive-tree-delete');
+
+    -- first, find all all descendants of the node to delete:
+    delete from subtree;
+    insert into subtree (nid)
+      select below as nid from tree_path
+      where tree=old.tree and above=old.child;
+
+    -- delete all traces of these nodes from tree_path.
+    -- we don't need to consider the 'above' column because
+    -- that's already covered by the notion of 'subtree'.
+    delete from tree_path
+      where tree = old.tree
+        and below in (select nid from subtree);
+
+    -- now we can 'garbage collect' the subtree from the
+    -- main tree table. this would trigger recursion if it
+    -- weren't for the flag we set.
+    delete from tree_data
+    where tree = old.tree
+      and child in (select nid from subtree);
+
+    -- finally, clean up our mess:
+    delete from flags where flag = 'recursive-tree-delete';
+    delete from subtree;
+  end;
+
+create trigger tree_prevent_child_mod before update of child on tree_data
+  begin
+    select raise (abort,
+      'update of tree_data.child prohibited. delete and re-add instead.');
+  end;
+
+create trigger tree_prevent_tree_mod before update of tree on tree_data
+  begin
+    select raise (abort,
+      'update of tree_data.tree prohibited. delete and re-add instead.');
+  end;
+
+create trigger tree_move_node after update of parent on tree_data
+  when new.parent is not null
+  begin
+    -- this technique is derived from:
+    --       http://jdobbie.blogspot.com/2009/07/closure-trees.html
+    --   and http://www.mysqlperformanceblog.com/2011/02/14/moving-subtrees-in-closure-table/
+    -- but adapted for sqlite, which seems to have a more flexible syntax than mysql.
+    delete from subtree;
+    insert into subtree
+      select below from tree_path
+      where tree=old.tree and above=old.child;
+
+    -- first we delete any 'old' ancestors for our subtree:
+    delete from tree_path
+      where tree = old.tree
+        and above in (select below from subtree)
+        and below not in (select below from subtree);
+
+    -- now, create some new ancestors in their place:
+    insert into tree_path (above, below, steps)
+    select up.above,
+           dn.below,
+           up.steps + dn.steps + 1 as steps
+      from tree_path as up cross join tree_path as dn
+     where up.tree = old.tree
+       and dn.tree = old.tree
+       and dn.above = new.child
+       and up.below = new.parent;
+
+     -- clean up:
+     delete from subtree;
+  end;
 
 create table edge (
   eid integer primary key,
