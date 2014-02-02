cc623a24b0d02c192924587960282adbff46974e
tangentstorm <michal.wallace@gmail.com>
2014-02-02 05:45:50 -0600

    uww: various bugfixes

--- uww.pas
+++ uww.pas
@@ -1,16 +1,18 @@
-{$mode delphi}
+{$i xpc.inc}{$mode delphi}
 unit uww;
-interface uses classes, ugeom2d;
+interface uses xpc, classes, ugeom2d, cw{for debug};
 
 type
   TWordWrap = class (TComponent)
     private
+      _gw,                 // gap width (space between items)
       _cx, _cy,            // current x, y
       _ww, _lh : cardinal; // wrap width and line height
     published
       constructor create(aOwner : TComponent); override;
       procedure reset;
       procedure place(item : ugeom2d.IBounds2D);
+      procedure debugdraw;
       property width : cardinal read _ww write _ww;
     end;
 
@@ -19,7 +21,7 @@ implementation
 constructor TWordWrap.Create(aOwner : TComponent);
   begin
     inherited create(aOwner);
-    _cx := 0; _cy := 0; _ww := 64; _lh := 1;
+    _cx := 0; _cy := 0; _ww := 64; _lh := 1; _gw := 1;
   end;
 
 procedure TWordWrap.Reset;
@@ -28,17 +30,21 @@ procedure TWordWrap.Reset;
   end;
 
 procedure TWordWrap.Place(item : ugeom2d.IBounds2D);
+  var gap : byte;
   begin //  prove this word wrap algorithm works
-    { wrap when item is wider than the distance to the edge. }
-    if (item.w > _ww - _cx) then
-      begin
-	if (_cx<>0) then _cy += _lh;
+    if _cx = 0 then gap := 0 else gap := _gw;
+    if (item.w + gap) >= (_ww - _cx - 1) then
+      begin { item is wider than distance to edge, so wrap. }
+	if (_cx = 0) then ok else _cy += _lh;
 	_cx := 0; item.x := 0; item.y := _cy;
       end
-    else
-      begin
-	item.x := _cx; _cx += item.w;
-      end
+    else item.x := _cx + gap;
+    _cx += item.w + gap;
+  end;
+
+procedure TWordWrap.debugdraw;
+  begin
+    cxy(1, _cx, _cy, '>')
   end;
 
 initialization
