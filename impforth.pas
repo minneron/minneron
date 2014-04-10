fc0ba8ba3264b59fd27dc26a0b309a3fedff3c0b
tangentstorm <michal.wallace@gmail.com>
2014-04-10 17:42:14 -0500

    you can now command a TTermView directly from ImpShell!

--- impforth.pas
+++ impforth.pas
@@ -1,6 +1,6 @@
 {$mode delphiunicode}{$i xpc.inc}
 program impforth;
-uses xpc, uapp, ukm, ui, cw, kvm, cli,
+uses xpc, classes, uapp, ukm, ui, utv, cw, kvm, cli,cx,
   uimpforth, uimpwords, uimpshell;
 
 type
@@ -8,6 +8,7 @@ type
     public
       imp : TImpForth;
       cmd : TImpShell;
+      scr : utv.TTermView;
       procedure Init; override;
       procedure Keys(km : ukm.TKeyMap); override;
       procedure Step; override;
@@ -15,28 +16,35 @@ type
     end;
 
 procedure TForthApp.init;
+  var term : TImpTerm;
   begin
-    imp := TImpForth.Create(self); TImpWords.Create(imp);
-    imp.AddOp('bye', quit);
-    imp.OnChange := self.OnImpChange;
+    imp := TImpForth.Create(self);
+    term := TImpTerm.Create(imp); term.view.resize(64, 16);
+    scr := term.view;
+    with imp do begin
+      addOp('bye', quit);
+      mount('term', term);
+      mount('forth', TForthWords);
+      OnChange := OnImpChange;
+    end;
     cmd := TImpShell.Create(self, imp);
     // --TODO : tview/zobj.center --------
     cmd.x := self.w div 2 - cmd.w div 2;
     cmd.y := self.h div 2 - cmd.h div 2;
     // -----------------------------------
-    _views.extend([cmd]);
+    _views.extend([cmd, scr]);
     kvm.clrscr;
   end;
 
 procedure TForthApp.OnImpChange;
-  begin
-    cmd.smudge;
+  begin cmd.smudge;
   end;
 
 procedure TForthApp.keys(km : ukm.TKeyMap);
   begin
     cmd.keys(km);
     km.cmd[ ^C ] := quit;
+    km.cmd[ ^L ] := scr.smudge;
   end;
 
 procedure TForthApp.step;
--- uimpwords.pas
+++ uimpwords.pas
@@ -1,56 +1,105 @@
 // this implements the a small core of basic forth words
 // using free pascal variants for cells.
 
-{$mode delphiunicode}
+{$mode delphiunicode}{$i xpc.inc}
 unit uimpwords;
-interface uses xpc, classes, kvm, uimpforth;
-
+interface uses xpc, classes, kvm, utv, uimpforth;
+
 type
-  TImpWords = class (TComponent)
-    protected
-      data, side : TimpStack;
-      imp : TImpForth;
-    public
-      constructor Create(aImp :  TImpForth); reintroduce;
+  TForthWords = class (TImpModule)
+   published
+      procedure Attach; override;
       procedure OpADD;
       procedure OpSUB;
       procedure OpMUL;
       procedure OpDIV;
-    end;
+   end;
 
-implementation
+  TImpTerm = class (TImpModule)
+    public term : kvm.ITerm; view : utv.TTermView;
+      constructor Create(aOwner : TComponent); override;
+      procedure attach; override;
+      procedure gettextattr; procedure getw; procedure geth;
+      procedure settextattr;
+    published { this is just kvm.ITerm, but using the stack }
+      procedure xMax; procedure yMax;
+      procedure xCur; procedure yCur;
+      procedure xPos; procedure yPos;
+      procedure clrscr; procedure clreol;
+      procedure newln; procedure scrollup;
+      procedure fg; procedure bg; procedure emit; procedure goxy;
+      procedure insln; procedure delln;
+      procedure resize;
+    end;
 
-constructor TImpWords.Create(aImp : TImpForth);
+implementation
+
+procedure TForthWords.Attach;
   begin
-    inherited Create(aImp);
-    imp := aImp; data := imp.data; side := imp.side;
     imp.AddOp('drop', imp.data.drop);
     imp.AddOp('swap', imp.data.swap);
-    imp.AddOp('clear', kvm.work.ClrScr);
     imp.AddOp('+', self.opADD);
     imp.AddOp('-', self.opSUB);
     imp.AddOp('*', self.opMUL);
     imp.AddOp('%', self.opDIV);
   end;
-
-procedure TImpWords.opADD;
+
+procedure TForthWords.opADD;
   begin data.push(data.pop + data.pop)
   end;
 
-procedure TImpWords.opMUL;
+procedure TForthWords.opMUL;
   begin data.push(data.pop * data.pop)
   end;
 
-procedure TImpWords.opSUB;
+procedure TForthWords.opSUB;
   var x, y : variant;
   begin data.pop2(x,y); data.push(y - x);
   end;
 
-procedure TImpWords.opDIV;
+procedure TForthWords.opDIV;
   var x, y : variant;
   begin data.pop2(x,y); data.push(y div x);
   end;
-
+
+constructor TImpTerm.Create(aOwner : TComponent);
+  begin inherited Create(aOwner);
+    view := utv.TTermView.Create(self); view.name := 'view'; term:=view.term;
+  end;
+procedure TImpTerm.Attach;
+  procedure ao(s:TStr;meth:TThunk);
+    begin imp.addop(s,meth) end;
+  begin ao('getw',getw); ao('geth',geth); ao('xPos',xPos); ao('yPos',yPos);
+    ao('xMax',xMax); ao('yMax',yMax); ao('xCur',xCur); ao('yCur',yCur);
+    ao('clrscr',clrscr); ao('clreol',clreol); ao('newln',newln);
+    ao('scrollup',scrollup); ao('fg',fg); ao('bg',bg); ao('emit',emit);
+    ao('insln',insln); ao('delln',delln); ao('goxy',goxy); ao('rsz',resize);
+  end;
+// accessors
+procedure TImpTerm.settextattr; begin term.textattr := data.pop end;
+procedure TImpTerm.gettextattr; begin data.push(term.textattr) end;
+procedure TImpTerm.getw; begin data.push(term.width) end;
+procedure TImpTerm.geth; begin data.push(term.height) end;
+procedure TImpTerm.xPos; begin data.push(view.x) end;
+procedure TImpTerm.yPos; begin data.push(view.y) end;
+// published method wrappers (they all just delegate to .term)
+procedure TImpTerm.xMax; begin data.push(term.xMax) end;
+procedure TImpTerm.yMax; begin data.push(term.yMax) end;
+procedure TImpTerm.xCur; begin data.push(term.wherex) end;
+procedure TImpTerm.yCur; begin data.push(term.wherey) end;
+procedure TImpTerm.clrscr; begin term.ClrScr; view.smudge end;
+procedure TImpTerm.clreol; begin term.ClrEol; view.smudge end;
+procedure TImpTerm.newln; begin term.NewLine; view.smudge end;
+procedure TImpTerm.scrollup; begin term.ScrollUp; view.smudge end;
+procedure TImpTerm.fg; begin term.fg(data.pop) end;
+procedure TImpTerm.bg; begin term.Bg(data.pop) end;
+procedure TImpTerm.emit; begin term.emit(data.pop); view.smudge end;
+procedure TImpTerm.insln; begin term.InsLine; view.smudge end;
+procedure TImpTerm.delln; begin term.DelLine; view.smudge end;
+procedure TImpTerm.goxy; var x,y : variant;
+  begin data.pop2(x,y); term.gotoxy(x,y) end;
+procedure TimpTerm.resize; var x,y : variant;
+  begin data.pop2(x,y); term.resize(x,y); end;
 
 initialization
 end.
